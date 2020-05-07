dc891cfa6cbe950eabeac1b162b8cc428cd96257
Bryce Gassner <61786753+Gass0014@users.noreply.github.com>
2020-05-07 13:51:22 +0930

    Updating Work

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.P07Prototype',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.P07Prototype\n\nA docassemble extension.\n\n## Author\n\nBryce Gassner, gass0014@flinders.edu.au\n\n',
+      long_description='# docassemble.P07Prototype\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nBryce Gassner, gass0014@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Bryce Gassner',
       author_email='gass0014@flinders.edu.au',
